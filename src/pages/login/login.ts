1016343df6ab5556f80be6262091883c3571daa9
Karl <krmouat22@gmail.com>
2018-04-09 23:23:01 +0200

    initial commit of boilerplate code

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the LoginPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { TabsPage } from '../tabs/tabs'
 
 @IonicPage()
 @Component({
@@ -21,5 +16,10 @@ export class LoginPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
+
+  login(){
+    this.navCtrl.push(TabsPage);
+  }
+
 
 }
