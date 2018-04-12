d4f1d49c3c3b72ff6e2befb4f9aa4eecde532002
Mathilde T <mathilde.thuringer@gmail.com>
2018-04-12 17:05:06 +0200

    Fixed

---
+++ src/pages/studentappointment/studentappointment.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NavController, App, MenuController } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
+
+
+@Component({
+  selector: 'page-studentappointment',
+  templateUrl: 'studentappointment.html'
+})
+export class StudentAppointmentPage {
+
+  constructor(public navCtrl: NavController, public app: App, menu: MenuController) {
+   
+      menu.enable(true);
+  }
+
+  
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.navCtrl.setRoot(page.component);
+  }
+
+  takeAnAppointment(){
+        this.navCtrl.push(TabsPage);
+      }
+
+}
